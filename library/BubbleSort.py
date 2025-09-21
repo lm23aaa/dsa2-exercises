953ae4fab8f46bba7d8bd86cb892a5703a0de064
Liam Mills <lm23aaa@herts.ac.uk>
2025-09-21 16:35:56 +0100

    feat: Add SelectionSort & BubbleSort functions to the library for unit 2.4

---
+++ library/BubbleSort.py
@@ -0,0 +1,17 @@
+def BubbleSort(list):
+    has_swapped = True
+
+    while (has_swapped == True):
+        has_swapped = False
+
+        for i in range(0, len(list)):
+            if i + 1 != len(list) and list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+                has_swapped = True
+    
+    print(list)
+    return list
+
+# BubbleSort(["London", "Canterbury", "York", "Leicester"])
+# BubbleSort([3,4,90,75,382,2,8,3,55,32,1,19,0,11])
+# BubbleSort([0.5,0.01,0.001,0.0001,0.7,0.02])
---
+++ library/SelectionSort.py
@@ -0,0 +1,80 @@
+def SelectionSort(list):
+    # Based on psuedocode from DSA2 2.4, amended to sort by asc alphabetically,
+    # or asc numerically
+    # https://herts.instructure.com/courses/123373/pages/2-dot-4-sorting-algorithms
+    list = list.copy()
+    result = []
+    n = len(list)
+    number_of_steps = 0
+
+    for i in range(0, n):
+        minimum_element = list[0]
+
+        for j in range(0, n):
+            if (list[j] < minimum_element):
+                minimum_element = list[j]
+
+            number_of_steps = number_of_steps + 1
+
+        result.append(minimum_element)
+        list.remove(minimum_element)
+        n -= 1
+    
+    print(result)
+    # print(number_of_steps)
+    return result
+
+def SelectionSortAlt(list):
+    # Based on psuedocode from DSA1 6.2.2, sorting by asc alphabetically,
+    # or asc numerically
+    # https://herts.instructure.com/courses/118171/pages/6-dot-2-2-selection-sort
+    n = 0
+    number_of_steps = 0
+    list = list.copy()
+
+    while n < len(list) - 1:
+        for i in range(n + 1, len(list)):
+            if list[n] > list[i]:
+                list[n], list[i] = list[i], list[n]
+
+            number_of_steps = number_of_steps + 1
+        n += 1
+    
+    print(list)
+    # print(number_of_steps)
+    return list
+
+def SelectionSortWordLength(list):
+    # Based on psuedocode from DSA1 6.2.2, sorting by asc word length
+    # https://herts.instructure.com/courses/118171/pages/6-dot-2-2-selection-sort
+    list = list.copy()
+    result = []
+    n = len(list)
+    number_of_steps = 0
+
+    for i in range(0, n):
+        minimum_length = len(list[0])
+        minimum_element = list[0]
+        
+        for j in range(0, n):
+            if (len(list[j]) < minimum_length):
+                minimum_length = len(list[j])
+                minimum_element = list[j]
+
+            number_of_steps = number_of_steps + 1
+
+        result.append(minimum_element)
+        list.remove(minimum_element)
+        n = n - 1
+
+    print(result)
+    # print(number_of_steps)
+    return result
+
+
+# list = ["London", "Canterbury", "York", "Leicester"]
+# list = [3, 1, 4, 2]
+# list = [0.5,0.01,0.001,0.0001,0.7,0.02]
+# SelectionSortAlt(list)
+# SelectionSort(list)
+# SelectionSortWordLength(list)
