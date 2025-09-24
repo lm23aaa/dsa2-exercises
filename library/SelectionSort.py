d954c220a82687ae1f5eeba2971213fe2e0dede0
Liam Mills <lm23aaa@herts.ac.uk>
2025-09-24 18:54:40 +0100

    fix: alter selection sort functions to duplicate array and not work on the original

--- library/SelectionSort.py
+++ library/SelectionSort.py
@@ -24,13 +24,13 @@ def SelectionSort(list_parameter):
     # print(number_of_steps)
     return result
 
-def SelectionSortAlt(list):
+def SelectionSortAlt(list_parameter):
     # Based on psuedocode from DSA1 6.2.2, sorting by asc alphabetically,
     # or asc numerically
     # https://herts.instructure.com/courses/118171/pages/6-dot-2-2-selection-sort
     n = 0
     number_of_steps = 0
-    list = list.copy()
+    list = list_parameter.copy()
 
     while n < len(list) - 1:
         for i in range(n + 1, len(list)):
@@ -44,10 +44,10 @@ def SelectionSortAlt(list):
     # print(number_of_steps)
     return list
 
-def SelectionSortWordLength(list):
+def SelectionSortWordLength(list_parameter):
     # Based on psuedocode from DSA1 6.2.2, sorting by asc word length
     # https://herts.instructure.com/courses/118171/pages/6-dot-2-2-selection-sort
-    list = list.copy()
+    list = list_parameter.copy()
     result = []
     n = len(list)
     number_of_steps = 0
