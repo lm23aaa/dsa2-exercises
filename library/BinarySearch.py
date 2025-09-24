e6fecc71f16829e7d4aa1beb721bf3ac5d9bae26
Liam Mills <lm23aaa@herts.ac.uk>
2025-09-24 18:32:31 +0100

    fix: rename variable to make more sense with the binary search methodology

--- library/BinarySearch.py
+++ library/BinarySearch.py
@@ -4,17 +4,17 @@ def BinarySearch(list_parameter, target):
 
     while not found and len(numbers) > 0:
         index = int(len(numbers) / 2)
-        current_item = int(numbers[index])
+        middle_item = int(numbers[index])
 
-        if current_item == target:
+        if middle_item == target:
             print(f"I found the number {str(target)} in the list.")
             found = True
             return found
         
-        if current_item < target:
+        if middle_item < target:
             numbers = numbers[(index + 1):len(numbers)]
 
-        elif current_item > target:
+        elif middle_item > target:
             numbers = numbers[0:index]
     
     print(f"Number {str(target)} was not found in the sorted list of numbers.")
