a54abde8418a72b3c55a706c85aac94920b64b4b
Liam Mills <lm23aaa@herts.ac.uk>
2025-10-04 15:20:04 +0100

    feat: amend BinarySearch function so that it returns the index value

--- exercises/3-1-fibonacci.py
+++ exercises/3-1-fibonacci.py
@@ -1,3 +1,5 @@
+from time import perf_counter
+
 # 2. fibonacci
 # n <  0, return -1 (ERROR CODE)
 # n == 0, return 0
@@ -16,3 +18,48 @@ def fibonacci(number):
 # while i < 10:
 #     print(f"{i}: {fibonacci(i)}")
 #     i += 1
+
+def fib_dynamic(number, memo):
+    if len(memo) > number and memo[number] != None:
+        return memo[number]
+    
+    if number < 0:
+        return -1
+    elif number == 0 or number == 1:
+        memo.insert(number, number)
+        return number
+    else:
+        curr = fibonacci(number - 2) + fibonacci(number - 1)
+        memo.insert(number, curr)
+        return curr
+    
+def fib_ground_up(number):
+    if number < 0:
+        return -1
+    elif number == 0 or number == 1:
+        return number
+    
+    memo = [1, 1]
+    for n in range(2, number + 1):
+        memo.insert(n, memo[n - 2] + memo[n - 1])
+    
+    return memo[number]
+
+def fib_timer(number, type = 'rec'):
+    start = perf_counter()
+    
+    if type == 'rec':
+        fibonacci(number)
+    elif type == 'dyn':
+        memo = []
+        fib_dynamic(number, memo)
+    else:
+        fib_ground_up(number)
+        
+    stop = perf_counter()
+    
+    print(stop - start)
+
+fib_timer(35)
+fib_timer(35, 'dyn')
+fib_timer(35, 'gro')
--- library/BinarySearch.py
+++ library/BinarySearch.py
@@ -1,4 +1,8 @@
-def BinarySearch(list_parameter, target):
+def BinarySearchOriginal(list_parameter = [], target = 0):
+    if (len(list_parameter) == 0):
+        print(f"Number {str(target)} was not found in the sorted list of numbers.")
+        return -1
+    
     found = False
     numbers = list_parameter.copy()
 
@@ -19,6 +23,44 @@ def BinarySearch(list_parameter, target):
     
     print(f"Number {str(target)} was not found in the sorted list of numbers.")
     return found
+
+def BinarySearch(list_parameter = [], target = 0):
+    """
+    Returns the index of a number if it is present in an SORTED list of
+    ints, using the binary search algorith.
+    E.g         valid   BinarySearch([1,2,4],4) = 2
+                        BinarySearch([-1,0,3,4,56],0) = 1
+                invalid BinarySearch([],5) = -1
+                        BinarySearch([-1,0,3,4,56],70) = -1
+    @param      list_paramter, SORTED list of ints
+    @param      target, int which is the taget value of the search
+    @returns    index value of the target in the array, or -1 if the value
+                is not present, or if the list_parameter is empty
+    """
+
+    if (len(list_parameter) == 0):
+        print(f"Number {str(target)} was not found in the sorted list of numbers.")
+        return -1
+
+    start = 0
+    end = len(list_parameter) - 1
+
+    while start <= end:
+        middle = int((start + end) / 2)
+        curr = int(list_parameter[middle])
+
+        if curr == target:
+            print(f"I found {str(target)} at index {str(middle)} in the list.")
+            return middle
+        
+        if curr < target:
+            start = middle + 1
+
+        elif curr > target:
+            end = middle - 1
+    
+    print(f"Number {str(target)} was not found in the sorted list of numbers.")
+    return -1
 
 def BinarySearchUserInput():
     numbers = input("Please enter a comma separated, sorted list of integers i.e. -1,0,3,4,56 etc: ").split(',')
